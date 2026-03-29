2b5d294ba6f5c4b5773b8daabde7409f8c54f06b
Hendrik Wedemeier <hendrik.wedemeier@hotmail.de>
2026-03-29 17:45:03 +0200

    cleaned up main

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,11 @@
 use minifb::{Window, WindowOptions};
-use crate::vector_math::vector2d::Vector2d;
+use crate::{vector_math::vector2d::Vector2d, window::window_manager::WindowManager};
 mod vector_math;
-
-const WIDTH: usize = 800;
-const HEIGHT: usize = 600;
-
+mod window;
 fn main() {
-    let mut buffer : Vec<u32> = vec![0; WIDTH * HEIGHT];
-
-    let mut window = Window::new(
-        "Pixel Pusher",
-        WIDTH,
-        HEIGHT,
-        WindowOptions::default(),
-    ).unwrap();
-
-    //roter pixel#
-    //buffer[30 * WIDTH + 80] = 0x000000FF;
-
-    update_pixel(&mut buffer, 80, 30, 0x000000FF);
-    while window.is_open() {
-        window.update_with_buffer(&buffer, WIDTH, HEIGHT).unwrap();
-    }
+    WindowManager::window_superloop();
 }
 
-fn update_pixel(buffer: &mut Vec<u32>, x: usize, y: usize, colour: u32){
-    buffer[y * WIDTH + x] = colour;
-}
 
 fn calc_vector(start: Vector2d, end: Vector2d){
     
--- src/window/window_manager.rs
+++ src/window/window_manager.rs
@@ -7,6 +7,16 @@ enum WindowType{
     fullscreen
 }
 
+pub struct WindowBuffer{
+    buffer: Vec<u32>
+}
+
+impl WindowBuffer{
+    fn new(width: usize, height: usize) -> Self{
+        Self { buffer: vec![0; width * height] }
+    }
+}
+
 pub struct WindowManager{
     width: usize,
     height: usize,
@@ -20,31 +30,57 @@ impl WindowManager{
     pub fn get_instance() -> MutexGuard<'static, WindowManager> {
         INSTANCE
             .get_or_init(|| Mutex::new(WindowManager {
-                width: get_window_width(),
-                height: get_window_height(),
-                _type: get_window_type(),
+                width: WindowManager::get_window_width(),
+                height: WindowManager::get_window_height(),
+                _type: WindowManager::get_window_type()
+
             }))
             .lock()
             .unwrap()
     }
 
-    fn new(width: usize, height: usize, _type : WindowType) -> Self{
+    fn new(width: usize, height: usize, _type: WindowType) -> Self{
         Self{width, height, _type}
     }
 
-    fn window_superloop(){
+    pub fn window_superloop(){
 
+        let mut buffer = 
+        WindowBuffer::new(WindowManager::get_window_width(), WindowManager::get_window_height())
+        .buffer;
+        let mut window_instance = WindowManager::get_instance();
+
+        let mut window = Window::new(
+        "Pixel Pusher",
+        window_instance.width,
+        window_instance.height,
+        WindowOptions::default(),
+    ).unwrap();
+
+    WindowManager::update_pixel(&mut buffer, 80, 30, 0x000000FF);
+    while window.is_open() {
+        window.update_with_buffer(&buffer, window_instance.width, window_instance.height).unwrap();
+    }
     }
 
-    fn get_window_height() -> i32{
-
+    fn get_window_height() -> usize{
+        return 600;
     }
 
-    fn get_window_width() -> i32{
-
+    fn get_window_width() -> usize{
+        return 800;
     }
 
     fn get_window_type() -> WindowType{
-
+        return WindowType::windowed;
     }
+
+    fn set_buffer() -> Vec<u32>{
+        let buffer: Vec<u32> = vec![0; WindowManager::get_window_width() * WindowManager::get_window_height()];
+        return buffer;
+    }
+
+    fn update_pixel(buffer: &mut Vec<u32>, x: usize, y: usize, colour: u32){
+    buffer[y * WindowManager::get_window_width() + x] = colour;
+}
 }
