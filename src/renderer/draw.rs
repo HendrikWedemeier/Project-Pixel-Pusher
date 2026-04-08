f8dd7f93983e816ed9e4e121df67d56651f002ae
Hendrik Wedemeier <hendrik.wedemeier@hotmail.de>
2026-04-08 23:48:32 +0200

    fixed wrong itertaion in draw

--- src/renderer/draw.rs
+++ src/renderer/draw.rs
@@ -6,7 +6,7 @@ pub mod Draw{
         let length = Vector2d::get_length(start_point, end_point);
 
         let mut point = Vector2d::new(0.0, 0.0);
-        for i in 1..=length as i32{
+        for i in 0..=length as i32{
             let lambda = i as f64 / length;
             point = Vector2d::add(start_point, &Vector2d::new(v.x * lambda, v.y * lambda));
             WindowManager::update_pixel(pixel_buffer, point.x as usize, point.y as usize, colour);
